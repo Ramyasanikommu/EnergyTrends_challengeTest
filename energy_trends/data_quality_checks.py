e2e49373f02e97dab8dab18edf209685e8a7923d
Ramyasanikommu <112522520+Ramyasanikommu@users.noreply.github.com>
2023-02-17 13:29:30 +0000

    Update data_quality_checks.py

--- energy_trends/data_quality_checks.py
+++ energy_trends/data_quality_checks.py
@@ -6,67 +6,67 @@ import pandas as pd
 BASE_PATH = Path(__file__).parent.parent
 
 
-def get_data_profiling(data_csv_path):
+def GetDataProfiling(data_csv_path):
     df = pd.read_csv(data_csv_path)
 
     stats = df.describe()
-    transposed_stats = stats.T
-    transposed_stats["Quarter"] = transposed_stats.index
-    transposed_stats["missing_count"] = df.isna().sum().sum()
-    transposed_stats["median"] = df.median(axis=0, skipna=True, numeric_only=True)
-    print(transposed_stats.head())
-    print(transposed_stats.columns)
-    stats_results = transposed_stats[['Quarter', 'count', 'min', 'max', 'mean', 'median', 'std']]
-    return stats_results
+    Transposedstats = stats.T
+    Transposedstats["Quarter"] = Transposedstats.index
+    Transposedstats["Missingcount"] = df.isna().sum().sum()
+    Transposedstats["Median"] = df.median(axis=0, skipna=True, numeric_only=True)
+    print(Transposedstats.head())
+    print(Transposedstats.columns)
+    StatsResults = Transposedstats[['Quarter', 'Count', 'Min', 'Max', 'Mean', 'Median', 'STD']]
+    return StatsResults
 
 
-def timeformat_check(current_report_df, column_names: list, format: str = "%Y-%m-%d %H:%M:%S"):
+def Timeformatcheck(current_report_df, column_names: list, format: str = "%Y-%m-%d %H:%M:%S"):
     results = {}
     for column_name in column_names:
-        processed_ts_values = current_report_df[column_name].unique().tolist()
-        time_format_check = "Success"
-        for timestamp_value in processed_ts_values:
+        ProcessedTsValues = current_report_df[column_name].unique().tolist()
+        Time_format_check = "Success"
+        for timestamp_value in ProcessedTsValues:
             if isinstance(datetime.datetime.strptime(timestamp_value, format), datetime.datetime) is False:
-                time_format_check = "Failed"
+                Time_format_check = "Failed"
                 break
-        results.update({column_name: time_format_check})
+        results.update({column_name: Time_format_check})
     return results
 
 
-def get_previous_rpt_columns():
-    reports_dir = os.path.join(BASE_PATH, "reports")
-    previous_rpt_columns = []
-    for file in os.listdir(reports_dir):
+def GetPreviousRptColumns():
+    Reports_dir = os.path.join(BASE_PATH, "reports")
+    Previous_rpt_columns = []
+    for file in os.listdir(Reports_dir):
         if "_data_" in file:
             continue
         else:
-            df = pd.read_csv(os.path.join(reports_dir, file))
+            df = pd.read_csv(os.path.join(Reports_dir, file))
             columns = df.columns
-        previous_rpt_columns.extend(columns)
-    unique_previous_cols = set(previous_rpt_columns)
-    return unique_previous_cols
+        Previous_rpt_columns.extend(columns)
+    Unique_previous_cols = set(Previous_rpt_columns)
+    return Unique_previous_cols
 
 
-def missing_cols_from_previous(current_report_df):
-    missing_cols = get_previous_rpt_columns().difference(set(current_report_df.columns))
-    return missing_cols
+def MissingColsFromPrevious(current_report_df):
+    Missing_cols = GetPreviousRptColumns().difference(set(current_report_df.columns))
+    return Missing_cols
 
 
-def new_cols_from_previous(current_report_df):
-    new_cols = set(current_report_df.columns).difference(get_previous_rpt_columns())
-    return new_cols
+def NewColsFromPrevious(current_report_df):
+    New_cols = set(current_report_df.columns).difference(GetPreviousRptColumns())
+    return New_cols
 
-def data_consistency_checks(data_csv_path):
+def DataConsistencyChecks(data_csv_path):
     df = pd.read_csv(data_csv_path)
-    timeformat_checks = timeformat_check(df, ["processed_at"])
-    record = {column + "_" + "time_format_check": status for column, status in timeformat_checks.items()}
-    missing_cols = missing_cols_from_previous(df)
-    if len(missing_cols) == 0:
-        missing_cols = ["ALL_GOOD"]
-    new_cols = new_cols_from_previous(df)
-    if len(new_cols) == 0:
-        new_cols = ["ALL_GOOD"]
-    record.update({"missing_columns_from_previous": "|".join(missing_cols)})
-    record.update({"new_columns_from_previous": "|".join(new_cols)})
-    data_consistency_df = pd.DataFrame([record])
-    return data_consistency_df
+    Timeformatchecks = Timeformatcheck(df, ["processed_at"])
+    Record = {column + "_" + "Time_format_check": status for column, status in Timeformatchecks.items()}
+    Missing_cols = MissingColsFromPrevious(df)
+    if len(Missing_cols) == 0:
+        Missing_cols = ["ALL_GOOD"]
+    New_cols = NewColsFromPrevious(df)
+    if len(New_cols) == 0:
+        New_cols = ["ALL_GOOD"]
+    Record.update({"missing_columns_from_previous": "|".join(Missing_cols)})
+    Record.update({"new_columns_from_previous": "|".join(New_cols)})
+    Data_consistency_df = pd.DataFrame([Record])
+    return Data_consistency_df
